02c5fd6636885c670d237c6e4a973a80eed690e3
Alessandro Gnoli <tebemis@gmail.com>
2014-11-05 18:20:12 -0600

    Added quakes reporting form interface

--- bots/rq-sim-bot/rq-sim-bot.rb
+++ bots/rq-sim-bot/rq-sim-bot.rb
@@ -1,10 +1,7 @@
+=begin
 require 'simple_ruby_mqtt_client'
 require 'json'
 require 'set'
-
-# Parse arguments
-# Connect to MQTT
-# Implement higher level publish/subscribe/unsubscribe/sync_req/async_req
 
 class String
   def is_json?
@@ -16,24 +13,34 @@ class String
   end
 end
 
-
 class Nutella
   
   def init(args)
     @run_id = args[0]
-    bot_name = @run_id + '_' + config_bot_name()
-    bot_name = bot_name[0, 23]
-    @sc = SimpleMQTTClient.new(args[1], bot_name)
+    @actor_name = config_actor_name(@run_id)
+    @sc = SimpleMQTTClient.new(args[1], @actor_name)
     @last_requests = Set.new
   end
-    
+  
+  # Subscribe to a channel
+  # The callback takes one parameter and that is the message that is received. 
+  # Messages that are not JSON are discarded.
   def subscribe (channel, callback)
     # Pad the channel
     new_channel = @run_id + '/' + channel
-    # Subscribe
-    @sc.subscribe(new_channel, callback)    
+    # Subscribe 
+    @sc.subscribe(new_channel, lambda do |message| 
+      # Make sure the message is JSON, if not drop the message
+      begin
+        message_hash = JSON.parse(message)
+        callback.call(message_hash)
+      rescue
+        return
+      end
+    end)    
   end
   
+  # Unsubscribe from a channel
   def unsubscribe(channel) 
     # Pad the channel
     new_channel = @run_id + '/' + channel
@@ -41,15 +48,27 @@ class Nutella
     @sc.unsubscribe(new_channel)
   end
   
+  # Publishes a message to a channel
+  # Message can be: 
+  # empty (equivalent of a GET)
+  # string (the string will be wrapped into a JSON string automatically. Format: {"payload":"<message>"})
+  # hash (the hash will be converted into a JSON string automatically)
+  # json string (the JSON string will be sent as is)
   def publish(channel, message)
+    # Verify that the message is proper JSON
     # Pad the channel
     new_channel = @run_id + '/' + channel
     # Publish
-    @sc.publish(new_channel, message)
+    begin
+      m = prepare_message_for_publish(message)
+      @sc.publish(new_channel, m)
+    rescue
+      STDERR.puts $!
+    end
   end
   
   # Performs a synchronosus request
-  # Message can be :
+  # Message can be:
   # empty (equivalent of a GET)
   # string (the string will be wrapped into a JSON string automatically. Format: {"payload":"<message>"})
   # hash (the hash will be converted into a JSON string automatically)
@@ -58,7 +77,12 @@ class Nutella
     # Generate message unique id
     id = message.hash 
     # Attach id
-    payload = attach_message_id(message, id)
+    begin
+      payload = attach_message_id(message, id)
+    rescue
+      STDERR.puts $!
+      return
+    end
     # Initialize response and response counter
     ready_to_go = 2
     response = nil
@@ -90,7 +114,12 @@ class Nutella
     # Generate message unique id
     id = message.hash 
     # Attach id
-    payload = attach_message_id(message, id)
+    begin
+      payload = attach_message_id(message, id)
+    rescue
+      STDERR.puts $!
+      return
+    end
     # Initialize flag that prevents handling of our own messages
     ready_to_go = false
     # Register callback to handle data the request response whenever it comes
@@ -132,13 +161,10 @@ class Nutella
       req.delete("id")
       res = handler.call(req)
       begin
-        if !res.is_a?(Hash)
-          raise 'When handling a request you always need to return a hash!'
-        end
-        res[:id]=id
-        publish(channel, res.to_json)
+        res_and_id = attach_message_id(res, id)
+        publish(channel, res_and_id)
       rescue
-        STDERR.puts $!
+        STDERR.puts 'When handling a request you need to retun JSON'
       end
     end)
   end
@@ -146,11 +172,11 @@ class Nutella
   
   private 
   
-  def config_bot_name 
+  def config_actor_name (run_id)
     h = JSON.parse( IO.read( "nutella.json" ) )
-    return h["name"]
+    full_actor_name = @run_id + '_' + h["name"]
+    full_actor_name[0, 23]
   end
-  
   
   def attach_message_id (message, id) 
     if message.is_a?(Hash)
@@ -162,27 +188,48 @@ class Nutella
       payload = p.to_json
     elsif message.is_a?(String)
       payload = { :payload => message, :id => id }.to_json
+    else
+      raise 'Your request is not JSON!'
+    end
+    payload
+  end
+  
+  def prepare_message_for_publish (message) 
+    if message.is_a?(Hash)
+      message[:from] = @actor_name
+      payload = message.to_json
+    elsif message.is_json?
+      p = JSON.parse(message)
+      message[:from] = @actor_name
+      payload = p.to_json
+    elsif message.is_a?(String)
+      payload = { :payload => message, :from => @actor_name }.to_json
+    else
+      raise 'You are trying to publish something that is not JSON!'
     end
     payload
   end
 end
+=end
 
+require 'nutella_lib'
 
 # Initialize nutella
-n = Nutella.new;
-n.init(ARGV)
+nutella.init ARGV
 
 # handle quakes schedule request
-n.handle_requests("quakes_schedule") do |req|
-  JSON.parse( IO.read( "db/quakes_schedule.json" ) )
-end
+# n.handle_requests("quakes_schedule") do |req|
+#   JSON.parse( IO.read( "db/quakes_schedule.json" ) )
+# end
 
 # handle room configuration request
-n.handle_requests("room_configuration") do |req|
-  JSON.parse( IO.read( "db/quakes_schedule.json" ) )
-end
+# n.handle_requests("room_configuration") do |req|
+#   JSON.parse( IO.read( "db/quakes_schedule.json" ) )
+# end
 
-
+# n.subscribe("schedule_update", lambda do |message|
+#   p message
+# end)
 
 begin
   while true
