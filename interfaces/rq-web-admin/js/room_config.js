8503ec3cdd1097bf92c7f50d45aa09df1d47ba58
Alessandro Gnoli <tebemis@gmail.com>
2014-11-13 20:57:31 -0600

    Frontend-backend synchronization completed

--- interfaces/rq-web-admin/js/room_config.js
+++ interfaces/rq-web-admin/js/room_config.js
@@ -13,8 +13,8 @@ var query_params = nutella.init(location.search, function() {
 	//Fetch room configuration
 	nutella.request("room_configuration", function(response) {
 		// Update model
-		room_height = response.room_height_inches;
-		room_width = response.room_width_inches;
+		room_height = response.room_height_meters;
+		room_width = response.room_width_meters;
 		seismographs = response.seismographs;
 		// If there are data, update the views, otherwise show the setup modal
 		if (seismographs===undefined) {
@@ -26,18 +26,6 @@ var query_params = nutella.init(location.search, function() {
 });
 
 
-// Utility function to update views
-function updateViews() {
-	// Update views (canvas)
-	edit_canvas.setAttribute("height", edit_canvas.width * room_height / room_width)
-	edit_p.size(edit_canvas.width, edit_canvas.height);
-	// Update views (table)
-	$(".seismographs_table tbody").empty();
-	seismographs.forEach(function(e) {
-		$(".seismographs_table tbody").append('<tr r_id="'+e.id+'"><td>'+e.id+'</td><td class="s_input_cell_x">'+e.x+'</td><td class="s_input_cell_y">'+e.y+'</td></tr>');
-	});
-	$(".seismographs_table tbody").append('<tr r_id="'+(seismographs.length+1)+'"><td>'+(seismographs.length+1)+'</td><td class="s_input_cell_x"> <input class="s_input_x" type="text" placeholder="meters" /> </td><td class="s_input_cell_y"> <input class="s_input_y" type="text" placeholder="meters" /> </td> </tr>'); 
-}
 
 
 // Add double click handlers to seismographs_table cells
@@ -61,20 +49,23 @@ $(".seismographs_table").on("input", ".s_input_x, .s_input_y", function() {
 	var cur_c;
 	// Store the value of "the other cell" (i.e. if we are editing x, retrieve y and vice versa)
 	if ($(this).hasClass("s_input_x")) {
-		cur_c = parseInt($(this).parent().parent().children(".s_input_cell_y").text());
+		cur_c = parseFloat($(this).parent().parent().children(".s_input_cell_y").text());
 		if (seismographs[cur_idx]!=undefined) {
-			seismographs[cur_idx].x = $(this).val();
+			seismographs[cur_idx].x = parseFloat($(this).val());
+			shipToBackend();
 		};
 	} else {
-		cur_c = parseInt($(this).parent().parent().children(".s_input_cell_x").text());
+		cur_c = parseFloat($(this).parent().parent().children(".s_input_cell_x").text());
 		if (seismographs[cur_idx]!=undefined) {
-			seismographs[cur_idx].y = $(this).val();
+			seismographs[cur_idx].y = parseFloat($(this).val());
+			shipToBackend();
 		};
 	}
 	// If we are editing the row right before the last one and we clear the input field we should delete the row below
 	if ($(this).val()=="" && parseInt(cur_row_id) == parseInt(last_row_id) - 1) {
 		seismographs.pop();
 		$(".seismographs_table tr[r_id='"+last_row_id+"']").remove();
+		shipToBackend();
 	}
 	// If we are editing the last row and we are typing we might have to add a new row
 	if (cur_row_id == last_row_id && !isNaN(cur_c)) {
@@ -83,13 +74,14 @@ $(".seismographs_table").on("input", ".s_input_x, .s_input_y", function() {
 		var s = {};
 		s.id = new_id - 1;
 		if ($(this).hasClass(".s_input_x")) {
-			s.x = parseInt($(this).val());
+			s.x = parseFloat($(this).val());
 			s.y = cur_c;
 		} else {
 			s.x = cur_c;
-			s.y = parseInt($(this).val());
+			s.y = parseFloat($(this).val());
 		}
 		seismographs.push(s);
+		shipToBackend();
 	}
 });
 
@@ -98,6 +90,8 @@ $(".seismographs_table").on("input", ".s_input_x, .s_input_y", function() {
 $(".seismographs_table").on("focusout", ".s_input_x, .s_input_y", function() {
 	var v = $(this).val();
 	var td = $(this).parent();
+	var tr = td.parent();
+	var idx = tr.attr("r_id")
 	if (v != "") {
 		$(this).remove();
 		td.text(v);
@@ -108,8 +102,8 @@ $(".seismographs_table").on("focusout", ".s_input_x, .s_input_y", function() {
 // Show seismographs setup modal once we click "Configure room" on room setup modal
 $("#roomSetupForm").on('valid.fndtn.abide', function() {
 	// Update model
-	room_width = parseInt($("#room_width_field").val())
-	room_height = parseInt($("#room_height_field").val());
+	room_width = parseFloat($("#room_width_field").val())
+	room_height = parseFloat($("#room_height_field").val());
 	// Update views (canvases)
 	setup_canvas.setAttribute("height", setup_canvas.width * room_height / room_width)
 	setup_p.size(setup_canvas.width, setup_canvas.height);
@@ -122,19 +116,37 @@ $("#roomSetupForm").on('valid.fndtn.abide', function() {
 
 // Dismiss seismographs setup modal once we click "setup seismographs" and ship all the information to the backend
 $("#seismographs_setup_btn").click(function() {
+	// Dismiss modal
+	$("#seismographsSetup").foundation("reveal", "close");
+	// Update view
+	updateViews();
 	// Ship to backend
+	shipToBackend();
+	return false;
+});
+
+
+
+// Utility function to update views
+function updateViews() {
+	// Update views (canvas)
+	edit_canvas.setAttribute("height", edit_canvas.width * room_height / room_width)
+	edit_p.size(edit_canvas.width, edit_canvas.height);
+	// Update views (table)
+	$(".seismographs_table tbody").empty();
+	seismographs.forEach(function(e) {
+		$(".seismographs_table tbody").append('<tr r_id="'+e.id+'"><td>'+e.id+'</td><td class="s_input_cell_x">'+e.x+'</td><td class="s_input_cell_y">'+e.y+'</td></tr>');
+	});
+	$(".seismographs_table tbody").append('<tr r_id="'+(seismographs.length+1)+'"><td>'+(seismographs.length+1)+'</td><td class="s_input_cell_x"> <input class="s_input_x" type="text" placeholder="meters" /> </td><td class="s_input_cell_y"> <input class="s_input_y" type="text" placeholder="meters" /> </td> </tr>'); 
+}
+
+// Utility function to ship stuff to backend whenever the model is modified
+function shipToBackend() {
 	var m = {}
 	m.room_width_meters = room_width;
 	m.room_height_meters = room_height;
 	m.seismographs = seismographs;
 	nutella.publish("room_config_update", m);
-	// Dismiss modal
-	$("#seismographsSetup").foundation("reveal", "close");
-	// Update view
-	updateViews();
-	return false;
-});
-
-
+}
 
 
