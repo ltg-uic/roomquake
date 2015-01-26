c6f371334ddacc52c8ae03ff6c30a7f30b07313c
Alessandro Gnoli <tebemis@gmail.com>
2015-01-26 13:06:06 -0600

    Wired aggregate view

--- bots/rq-knowledge-bot/rq-knowledge-bot.rb
+++ bots/rq-knowledge-bot/rq-knowledge-bot.rb
@@ -4,37 +4,33 @@ require 'nutella_lib'
 nutella.init ARGV
 
 # Initialize storage
-obs_store = nutella.persist.getJsonStore("db/observations.json")
+obs_store = nutella.persist.getJsonStore('db/observations.json')
 
 # Requests
 
 # handle observations requests
-nutella.net.handle_requests("observations") do |req|
+nutella.net.handle_requests('observations') do |req|
   obs_store.transaction { obs_store.to_h }
 end
 
 
 # Updates / Subscriptions
 
-# handle students observations updates
-nutella.net.subscribe("quake_reports", lambda do |m|
+# handle new students observations
+nutella.net.subscribe('new_observation', lambda do |m|
+  m.delete 'from'
   obs_store.transaction do
-    obs_array = obs_store["observations"]
-    obs_array[m["seismograph"]-1].push(m["distance"])
-    obs_store["observations"] = obs_array
+    obs_store['observations'] = Array.new if obs_store['observations'].nil?
+    obs_store['observations'].push(m)
   end
-end);
+end)
 
 # handle observations wiping
-nutella.net.subscribe("wipe_observations", lambda do |m|
-  s = room_store.transaction { room_store["seismographs"] }
+nutella.net.subscribe('wipe_observations', lambda do |m|
   obs_store.transaction do
-    obs_array = obs_store["observations"]
-    for i in 0..s.length
-      obs_array[i] = []
-    end
+    obs_store['observations'] = Array.new
   end
-end);
+end)
 
 
 # Just sit there waiting for messages to come
