1a1e8ed0b36c1e6e3f4619eddc32c79f219ff25d
tmoher <tmoher@users.noreply.github.com>
2017-11-05 19:11:08 -0600

    moving bg bot to node.js

--- bots/readings-bot/node-bot.js
+++ bots/readings-bot/node-bot.js
@@ -11,19 +11,97 @@ var nutella = NUTELLA.init(cliArgs.broker, cliArgs.app_id, cliArgs.run_id, compo
 var readings = nutella.persist.getJsonObjectStore('readings');
 readings.load();
 if (!readings.hasOwnProperty('data')){
-    readings = {data:[]};
+    readings.data =[];
     readings.save();
 }
 
 
-console.log ('Completed initialization. Read readings');
-console.log ('Listening for requests');
+var quakes_schedule = nutella.persist.getJsonObjectStore('quakes_schedule');
+quakes_schedule.load();
+if (!quakes_schedule.hasOwnProperty('quakes_schedule')){
+    quakes_schedule.quakes_schedule = [];
+    quakes_schedule.save();
+};
 
-// all files loaded
+var demo_quakes = nutella.persist.getJsonObjectStore('demo_quakes');
+demo_quakes.load();
+if (!demo_quakes.hasOwnProperty('demo_quakes')){
+    demo_quakes.quakes_schedule = [];
+    demo_quakes.save();
+};
 
-// open request handlers
 
-// these are used primarily by the distribution editor
+var room = nutella.persist.getJsonObjectStore('room_config');
+room.load();
+if (!room.hasOwnProperty('data')){
+    room.data = 
+                    {
+                        room_width_meters:10,
+                        room_height_meters:10,
+                        seismographs:[
+                                {id:1,x:0.1,y:9.9},
+                                {id:2,x:9,y:2},
+                                {id:3,x:3,y:3},
+                                {id:4,x:2,y:2},
+                                {id:5,x:7,y:8}
+                            ],
+                        rq_mode:"schedule"
+                    }
+    room.save();
+};
+
+nutella.net.handle_requests('room_configuration', function(message){
+    return room.data;
+});
+
+nutella.net.subscribe('room_config_update', function(message){
+    room = nutella.persist.getJsonObjectStore('room_config');
+    room.load();
+    room.data = message;
+    room.save();
+});
+
+
+nutella.net.subscribe('mode_update', function(message){
+
+    room = nutella.persist.getJsonObjectStore('room_config');
+    room.load();
+    room.data.rq_mode = message.rq_mode;
+    room.save();
+});
+
+nutella.net.handle_requests('quakes_schedule', function(message){
+    if (room.data.rq_mode == 'schedule') return quakes_schedule;
+    else return demo_quakes;
+});
+
+nutella.net.handle_requests('quakes_series', function(message){
+    return quakes_schedule;
+});
+
+nutella.net.subscribe('new_demo_quake', function(message){
+    demo_quakes = nutella.persist.getJsonObjectStore('demo_quakes');
+    demo_quakes.load();
+    var s = demo_quakes.quakes_schedule;
+    s.push(message);
+    demo_quakes.quakes_schedule = s;
+    demo_quakes.save();
+});
+
+nutella.net.subscribe('demo_quakes_clean', function(message){
+
+    demo_quakes = nutella.persist.getJsonObjectStore('demo_quakes');
+    demo_quakes.load();
+    demo_quakes.quakes_schedule = [];
+    demo_quakes.save();
+});
+
+nutella.net.subscribe('quakes_schedule_update', function(message){
+    quakes_schedule = nutella.persist.getJsonObjectStore('quakes_schedule');
+    quakes_schedule.load();
+    quakes_schedule = message;
+    quakes_schedule.save();
+});
 
 
 
@@ -32,10 +110,12 @@ nutella.net.handle_requests('get_readings', function (message, from){
 });
 
 
-//  nutella bug? workaround: must redefine and reload json objects prior to saving them
-//  
+// //  nutella bug? workaround: must redefine and reload json objects prior to saving them
+// //  
 
 nutella.net.subscribe('set_readings', function (message, from){
+    readings = nutella.persist.getJsonObjectStore('readings');
+    readings.load();
     readings.data = message;
     readings.save();
     // portals = nutella.persist.getJsonObjectStore('portals');
@@ -51,6 +131,8 @@ nutella.net.subscribe('set_readings', function (message, from){
 });
 
 nutella.net.subscribe('reading', function (message, from){
+    readings = nutella.persist.getJsonObjectStore('readings');
+    readings.load();
     readings.data.push(message);
     readings.save();
 });
--- bots/rq-backend-bot/rq-backend-bot.rb
+++ bots/rq-backend-bot/rq-backend-bot.rb
@@ -9,67 +9,67 @@ nutella.init(broker, app_id, run_id, component_id)
 
 
 # Initialize storage
-room_store = nutella.persist.get_json_object_store('room_config')
-quakes_store = nutella.persist.get_json_object_store('quakes_schedule')
-demo_quakes_store = nutella.persist.get_json_object_store('demo_quakes')
+#room_store = nutella.persist.get_json_object_store('room_config')
+# quakes_store = nutella.persist.get_json_object_store('quakes_schedule')
+# demo_quakes_store = nutella.persist.get_json_object_store('demo_quakes')
 
 
 # Requests
 
 # handle room configuration request
-nutella.net.handle_requests('room_configuration', lambda do |req, from|
-  room_store.to_h
-end)
+# nutella.net.handle_requests('room_configuration', lambda do |req, from|
+#   room_store.to_h
+# end)
 
 # handle quakes schedule request
-nutella.net.handle_requests('quakes_schedule', lambda do |req, from|
-  rq_mode = room_store['rq_mode']
-  rq_mode == 'schedule' ?  quakes_store.to_h : demo_quakes_store.to_h
-end)
+# nutella.net.handle_requests('quakes_schedule', lambda do |req, from|
+#   rq_mode = room_store['rq_mode']
+#   rq_mode == 'schedule' ?  quakes_store.to_h : demo_quakes_store.to_h
+# end)
 
 # handle quakes series exlclusively
-nutella.net.handle_requests('quakes_series', lambda do |req, from|
-  quakes_store.to_h
-end)
+# nutella.net.handle_requests('quakes_series', lambda do |req, from|
+#   quakes_store.to_h
+# end)
 
 
 
 # Updates / Subscriptions
 
 # handle room configuration updates
-nutella.net.subscribe('room_config_update', lambda do |m, f|
-  room_store.merge! m
-end)
+# nutella.net.subscribe('room_config_update', lambda do |m, f|
+#   room_store.merge! m
+# end)
 
 # handle mode switch
-nutella.net.subscribe('mode_update', lambda do |m, f|
-  room_store['rq_mode'] = m['rq_mode']
-end)
+# nutella.net.subscribe('mode_update', lambda do |m, f|
+#   room_store['rq_mode'] = m['rq_mode']
+# end)
 
 # handle new demo quakes
-nutella.net.subscribe('new_demo_quake', lambda do |m, f|
-  demo_quakes_store['quakes_schedule'] = Array.new if demo_quakes_store['quakes_schedule'].nil?
-  demo_quakes_store['quakes_schedule'] = demo_quakes_store['quakes_schedule'].push(m)
-end)
+# nutella.net.subscribe('new_demo_quake', lambda do |m, f|
+#   demo_quakes_store['quakes_schedule'] = Array.new if demo_quakes_store['quakes_schedule'].nil?
+#   demo_quakes_store['quakes_schedule'] = demo_quakes_store['quakes_schedule'].push(m)
+# end)
 
-# handle clear demo quakes
-nutella.net.subscribe('demo_quakes_clean', lambda do |m, f|
-  demo_quakes_store['quakes_schedule'] = Array.new
-end)
+# # handle clear demo quakes
+# nutella.net.subscribe('demo_quakes_clean', lambda do |m, f|
+#   demo_quakes_store['quakes_schedule'] = Array.new
+# end)
 
-# handle quakes schedule updates
-nutella.net.subscribe('quakes_schedule_update', lambda do |m, f|
-  quakes_store['quakes_schedule'] = m['quakes_schedule']
-end)
+# # handle quakes schedule updates
+# nutella.net.subscribe('quakes_schedule_update', lambda do |m, f|
+#   quakes_store['quakes_schedule'] = m['quakes_schedule']
+# end)
 
 # handle updates of current quake
-nutella.net.subscribe('set_current_quake', lambda do |m, f|
-  room_store['current_quake_id'] = m['current_quake_id']
-  room_store['current_quake_time'] = m['current_quake_time']
-end)
+# nutella.net.subscribe('set_current_quake', lambda do |m, f|
+#   room_store['current_quake_id'] = m['current_quake_id']
+#   room_store['current_quake_time'] = m['current_quake_time']
+# end)
 
 
-puts 'RQ Backend bot listening'
+# puts 'RQ Backend bot listening'
 
 # Just sit there waiting for messages to come
-nutella.net.listen
+# nutella.net.listen
